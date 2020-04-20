2d06acfac140cffb3b1f1c845db63887e01ce525
MkoniecOfficial <38956561+MkoniecOfficial@users.noreply.github.com>
2020-04-20 10:51:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,6 @@
 const { Client, RichEmbed } = require("discord.js");
 const { config } = require("dotenv");
 const market = require('steam-market-pricing');
-const​ ​config​ ​=​ ​require​(​"​./config.json​"​);
 
 const client = new Client({
     disableEveryone: true
@@ -13,22 +12,15 @@ config({
 
 client.on("ready", () => {
     console.log(`Hi, ${client.user.username} is now online!`);
+
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "me getting developed",
+            type: "WATCHING"
+        }
     }); 
 })
-
-
-bot.on("ready", async () => {
-  console.log(`${bot.user.username} is ready for action!`);
-  if (config.activity.streaming == true) {
-    bot.user.setActivity(config.activity.game, {url: 'https://twitch.tv/mkoniec'});
-  } else {
-    bot.user.setActivity(config.activity.game, {type:'WATCHING'}); //PLAYING, LISTENING, WATCHING
-    bot.user.setStatus('dnd'); //dnd, idle, online, invisible
-  }
-});
-
-
-
 
 client.on("message", async message => {
     const prefix = "!";
